f6164743642f7d340f37fc1844a56d462e58905d
MalamaKangwa <Malama.Kangwa@szi.gov.zm>
2020-01-08 15:00:45 +0200

    Task: Added Other Arithmetic

---
+++ InterfaceCalculator/Multiply.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations
+{
+    public class Multiply
+    {
+        public static int Product(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Product(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Product(double[] arrayA)
+        {
+            double c = arrayA[0];
+
+            foreach (double a in arrayA)
+            {
+                c = Product(a, c);
+            }
+
+            double d = c / arrayA[0];
+            return d;
+        }
+
+        public static int Product(int[] arrayB)
+        {
+            int d = 1;
+
+            foreach (int a in arrayB)
+            {
+                d = Product(a, d);
+            }
+            return d;
+        }
+
+    }
+}
---
+++ InterfaceCalculator/Subtraction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations
+{
+    public class Subtraction
+    {
+        public static int Difference(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static double Difference(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Difference(double[] arrayA)
+        {
+            double c = arrayA[0];
+
+            foreach (double a in arrayA)
+            {
+                c = Difference(c, a);
+            }
+            double d = c + arrayA[0];
+            return d;
+        }
+
+        public static int Difference(int[] arrayB)
+        {
+            int d = arrayB[0];
+            
+            foreach (int a in arrayB)
+            {
+                d = Difference(d, a);
+            }
+            int e = d + arrayB[0];
+            return e;
+        }
+    }
+}
