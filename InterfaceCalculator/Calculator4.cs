048caf57cb97ecbac89d79699119bdb4b9fb37d0
MalamaKangwa <Malama.Kangwa@szi.gov.zm>
2020-01-08 16:02:33 +0200

    Task: Linked Calculator to Interfaces

--- InterfaceCalculator/Calculator4.cs
+++ InterfaceCalculator/Calculator4.cs
@@ -9,10 +9,15 @@ namespace InterfaceCalculator
 
     }
 
-    public class Calculator4 : ISum, IDivide
+    public class Calculator4 : ISum, ISub, IMultiply, IDivide, ISquare, ISquareRoot, ICubed 
     {
         readonly AddingInterface adding = new AddingInterface();
+        readonly SubtractionInterface subbing = new SubtractionInterface();
         readonly DivisionInterface dividing = new DivisionInterface();
+        readonly MultiplyInterface multiplying = new MultiplyInterface();
+        readonly SquareInterface squaring = new SquareInterface();
+        readonly SquareRootInterface SqrRooting = new SquareRootInterface();
+        readonly CubedInterface cubing = new CubedInterface();
 
         public dynamic result;
 
@@ -22,11 +27,42 @@ namespace InterfaceCalculator
             return result;
         }
 
+        public dynamic Sub(dynamic a, dynamic b)
+        {
+            result = subbing.Sub(a, b);
+            return result;
+        }
+
+        public dynamic Multiplication(dynamic a, dynamic b)
+        {
+            result = multiplying.Multilplication (a, b);
+            return result;
+        }
+
         public dynamic Divide(dynamic a, dynamic b)
         {
             result = dividing.Divide(a, b);
             return result;
         }
+
+        public dynamic Sqr(dynamic a)
+        {
+            result = squaring.Sqr(a);
+            return result;
+        }
+
+        public dynamic SqrRoot(dynamic a)
+        {
+            result = SqrRooting.SqrRoot(a);
+            return result;
+        }
+
+        public dynamic CubedValue(dynamic a)
+        {
+            result = cubing.CubedValue(a);
+            return result;
+        }
+
 
 
     }
