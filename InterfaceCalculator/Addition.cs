ed22faf71ba7b29fe826d4aedc4e1110c463b7c7
MalamaKangwa <Malama.Kangwa@szi.gov.zm>
2020-01-08 14:22:10 +0200

    Task: Added Add Interface

---
+++ InterfaceCalculator/Addition.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace InterfaceCalculator
+{
+
+    public class Addition
+    {
+        public static int Sum (int a, int b)
+        {
+            return a + b;
+        }
+
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Sum(double[] arrayA)
+        {
+            double c = 0.0;
+
+            foreach (double a in arrayA) 
+            {
+                c = Sum(a, c);
+            }
+            return c;
+        }
+
+        public static int Sum(int[] arrayB)
+        {
+            int d = 0;
+
+            foreach (int a in arrayB)
+            {
+                d = Sum(a, d);
+            }
+            return d;
+        }
+
+    }
+}
