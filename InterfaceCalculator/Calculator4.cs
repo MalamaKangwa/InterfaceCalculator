6902711a6c2b0648b83af14a9e54cca0fdff950c
MalamaKangwa <Malama.Kangwa@szi.gov.zm>
2020-01-08 14:47:30 +0200

    Task: Added Calculator and Tests

---
+++ InterfaceCalculator/Calculator4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InterfaceCalculator
+{
+    public interface ICalc4
+    {
+            public dynamic Add(dynamic a, dynamic b);
+            public dynamic Divide(dynamic a, dynamic b);
+
+    }
+
+    public class Calculator4 : ISum, IDivide
+    {
+        readonly AddingClass adding = new AddingClass();
+        readonly DivClass dividing = new DivClass();
+
+        public dynamic result;
+
+        public dynamic Add(dynamic a, dynamic b)
+        {
+            result = adding.Add(a, b);
+            return result;
+        }
+
+        public dynamic Divide(dynamic a, dynamic b)
+        {
+            result = dividing.Divide(a, b);
+            return result;
+        }
+
+
+    }
+}
