b43140eff8a319e9aec70a8589fb48704423693a
MalamaKangwa <Malama.Kangwa@szi.gov.zm>
2020-01-09 15:23:21 +0200

    Task: Added Mean Logic

---
+++ AvgMean/MeanAvg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using InterfaceCalculator;
+
+namespace AvgMean
+{
+    public class MeanAvg
+    {
+        public static dynamic result;
+        public static dynamic Mean(dynamic arrayX)
+        {
+            var total = Addition.Sum(arrayX);
+            var array_Count = Helpers.ArrayHelper.Length(arrayX);
+            result = Division.Quotient(total, array_Count);
+            return result;
+        }
+    }
+}
---
+++ Mean_Interface/Mean_Interface.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Mean_Interface
+{
+    public interface IAvgMean
+    {   public dynamic Mean_Average(dynamic a);
+    }
+}
