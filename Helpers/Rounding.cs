8e765ec5622b0c3303a974ece8693956fcd4cc2b
MalamaKangwa <Malama.Kangwa@szi.gov.zm>
2020-01-09 14:00:01 +0200

    Task: Added Helpers

---
+++ Helpers/Rounding.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helpers
+{
+    class Rounding
+    {
+        public static dynamic RoundTwoDecimals(dynamic a)
+        {
+            return Math.Round(a, 2);
+        }
+    }
+}
